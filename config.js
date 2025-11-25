cffc9b73d8d9eaadcae8f84c3bca3f60d5a8bce7
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2025-11-24 23:00:21 -0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ SESSION_ID: process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiO
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Akashkavindu/ZANTA_MD/refs/heads/main/images/ChatGPT%20Image%20Nov%2021%2C%202025%2C%2001_21_32%20AM.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋...ZANTA-MD Is Alive Now😍*\n\n*You can contact me using this link*\n\nhttp://wa.me/+94743404814?text=*Hey__ZANTA*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/EChgJJtPHbY8IvrHApocWc*\n\n*You can join Our Whatsapp Chanel*\n\n*https://whatsapp.com/channel/0029VbBc42s84OmJ3V1RKd2B*\n\n> ZANTA MD WA BOT",
 BOT_OWNER: '94743404814',  // Replace with the owner's phone number
-
-
-
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || 'true', // 'true' හෝ 'false'
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || 'true',      // 'true' හෝ 'false'    
 };
